22d9c81cf8c30791341db6c7f9214dc77af49ead
Kumar, Abhishek <abhishek.kumar3@tyson.com>
2021-05-23 21:28:02 +0530

    Add Product Controller
    Basic crud option

---
+++ springboot-basics/src/main/java/com/send2abhishek/spring/springbootbasics/Product.java
@@ -0,0 +1,28 @@
+package com.send2abhishek.spring.springbootbasics;
+
+public class Product {
+	
+	private String id; 
+    private String title; 
+    private String desc; 
+    private String imagePath; 
+    private double unitPrice;
+    
+    
+	public String getId() {
+		return id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public String getDesc() {
+		return desc;
+	}
+	public String getImagePath() {
+		return imagePath;
+	}
+	public double getUnitPrice() {
+		return unitPrice;
+	}
+
+}
